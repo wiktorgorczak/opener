353ed57952ae39e495d1cf9f25622955bbc9753a
Wiktor Górczak <wiktor.gorczak@openanalytics.eu>
2021-12-10 19:18:55 +0100

    initialize spring project

---
+++ src/main/java/pl/hackyeah/szczepans/opener/OpenerApplication.java
@@ -0,0 +1,13 @@
+package pl.hackyeah.szczepans.opener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/hackyeah/szczepans/opener/OpenerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hackyeah.szczepans.opener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
