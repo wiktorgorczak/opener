1063f4e99bfa2752ebfa2ccd00918ffd815ddd31
Piotr Gleska <piotr.gleska@wp.pl>
2021-12-11 15:09:30 +0100

    add db

---
+++ src/main/java/pl/hackyeah/szczepans/opener/config/ApplicationConfig.java
@@ -0,0 +1,93 @@
+package pl.hackyeah.szczepans.opener.config;
+
+import java.util.Properties;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@Configuration
+public class ApplicationConfig {
+	
+	@Value("${db.driver}")
+	private String databaseDriver;
+	
+	@Value("${db.password}")
+	private String databasePassword;
+	
+	@Value("${db.url}")
+	private String databaseUrl;
+	
+	@Value("${db.username}")
+	private String databaseUsername;
+	
+	@Value("${hibernate.dialect}")
+	private String hibernateDialect;
+	
+	@Value("${hibernate.show_sql}")
+	private String hibernateShowSql;
+	
+	@Value("${hibernate.jdbc.time_zone}")
+	private String hibernateTimezone;
+	
+	@Value("${packages.to.scan}")
+	private String packagesToScan;
+	
+	@Resource
+	private Environment env;
+	
+	@Bean
+	public DataSource dataSource()	{
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(databaseDriver);
+		dataSource.setUrl(databaseUrl);
+		dataSource.setUsername(databaseUsername);
+		dataSource.setPassword(databasePassword);
+		
+		return dataSource;
+	}
+		
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+		LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactoryBean.setDataSource(dataSource());
+		entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+		entityManagerFactoryBean.setPackagesToScan(packagesToScan);
+		entityManagerFactoryBean.setJpaProperties(hibProperties());
+		
+		return entityManagerFactoryBean;
+	}	
+
+	@Bean
+	public JpaTransactionManager transactionManager() {
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());		
+		return transactionManager;
+	}
+	
+	private final Properties hibProperties() {
+		Properties properties = new Properties();
+		properties.put("hibernate.dialect",	hibernateDialect);
+		properties.put("hibernate.show_sql", hibernateShowSql);
+		properties.put("hibernate.jdbc.time_zone", hibernateTimezone);
+		
+		return properties;
+	}
+	
+    @Bean
+    public CharacterEncodingFilter characterEncodingFilter() {
+        final CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
+}
