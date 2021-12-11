73c8d2f5304aa3f4a20d04eabc23171529613f0c
Szczepaniak-M <m.szczepaniak99@gmail.com>
2021-12-11 18:41:58 +0100

    Fix Suffix mapping

--- src/main/java/pl/hackyeah/szczepans/opener/properties/MediaTypeSuffixProperties.java
+++ src/main/java/pl/hackyeah/szczepans/opener/properties/MediaTypeSuffixProperties.java
@@ -4,9 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.hackyeah.szczepans.opener.service.MediaTypeSuffixMapper;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,7 +15,10 @@ public class MediaTypeSuffixProperties {
     @Bean
     public MediaTypeSuffixMapper mediaTypeSuffixMapper() throws IOException {
         Map<String, String> map = new HashMap<>();
-        try (BufferedReader br = new BufferedReader(new FileReader("MediaTypeSuffix.config"))) {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream is = classloader.getResourceAsStream("MediaTypeSuffix.config");
+        InputStreamReader streamReader = new InputStreamReader(is, StandardCharsets.UTF_8);
+        try (BufferedReader br = new BufferedReader(streamReader)) {
             for (String line; (line = br.readLine()) != null; ) {
                 String[] splitLine = line.split(":");
                 map.put(splitLine[0], splitLine[1]);
